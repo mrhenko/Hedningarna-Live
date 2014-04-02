0f3dcb23b2500a498b2f9d027796455de0cc3021
Henrik Carlsson <henrik@henrikcarlsson.se>
2014-04-02 23:23:16 +0200

    Tuned the scene names filter.

--- SceneFilter.js
+++ SceneFilter.js
@@ -3,11 +3,14 @@ function filterName(args)  // Callback
 {
   var keyword=args.toString();
   var trackname=keyword.split("BPM ");
-  if (!trackname[1]) {
+  if (!trackname[0]) {
     var name = keyword;
 } else {
-    var name = trackname[1];
+    var name = trackname[0];
 }
+
+  name = name.replace(/\s(\s)+/g, " "); // Whitespace
+  name = name.replace(/[0-9]+/g, ""); // Numbers
   
   outlet(0, name);
 
